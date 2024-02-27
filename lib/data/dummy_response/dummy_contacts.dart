2f8b54c2916c7288600595016a2edfbef5ba33ec
JDKahle <jonnykahle@yahoo.de>
2024-02-27 17:52:36 +0100

    added intl to pubspec, beauty changes to Chats Page, created pretty date formatting widget

--- lib/data/dummy_response/dummy_contacts.dart
+++ lib/data/dummy_response/dummy_contacts.dart
@@ -1,8 +1,9 @@
 const Map<String, dynamic> dummyChatMessenges = {
   "messeges": [
     {
-      "message": "Hallo! Was geht? Wollte dir nur sagen dass ich Aids habe :(",
-      "time": "2022-01-01T23:56:00.000Z",
+      "message":
+          "Hallo! Was geht?\nWollte dir nur \nsagen dass ich Aids habe :(",
+      "time": "2024-02-26T23:56:00.000Z",
       "notificationCount": 5,
       "contact": {
         "id": "1",
@@ -12,25 +13,36 @@ const Map<String, dynamic> dummyChatMessenges = {
       }
     },
     {
-      "message": "This is another message",
-      "time": "2022-01-02T12:34:00.000Z",
-      "notificationCount": 2,
+      "message": "Hey,\nshow boobs pls",
+      "time": "2024-02-06T23:56:00.000Z",
+      "notificationCount": 17,
       "contact": {
         "id": "2",
-        "username": "Another Name",
+        "username": "Lord Faggot",
         "profileImageUrl":
             "https://images.paramount.tech/uri/mgid:arc:imageassetref:shared.southpark.us.en:871078f4-35f8-4ffd-9b26-156124e12e7d?quality=0.7&gen=ntrn&legacyStatusCode=true&format=jpg&width=1200&height=630&crop=true"
       }
     },
     {
       "message": "Ich bin ein Berliner",
-      "time": "2022-01-02T12:34:00.000Z",
+      "time": "1945-01-02T12:34:00.000Z",
       "notificationCount": 0,
       "contact": {
         "id": "3",
         "username": "Florian Schneider",
         "profileImageUrl":
             "https://images-ng.pixai.art/images/orig/eda76502-b4b2-402e-a2f9-e32bd61a0de6"
+      }
+    },
+    {
+      "message": "it's a me, Mario!",
+      "time": "1985-09-13T12:34:00.000Z",
+      "notificationCount": 120,
+      "contact": {
+        "id": "4",
+        "username": "Mario",
+        "profileImageUrl":
+            "https://external-preview.redd.it/g2LTo4JLv6KCT5oeoVPm-FcWVLmSi76i6aWK-S5vRAU.jpg?auto=webp&s=a0021b9985bc5822370ecdf61f22ad1b8d3b7882"
       }
     }
   ]
--- lib/presentation/pages/chat_contacts_page/widgets/chats_card_component.dart
+++ lib/presentation/pages/chat_contacts_page/widgets/chats_card_component.dart
@@ -9,6 +9,7 @@ import 'package:peer_app/presentation/whitelabel/components/types/size_types.dar
 import 'package:peer_app/presentation/whitelabel/constants.dart';
 import 'package:peer_app/presentation/whitelabel/icon_library.dart';
 import 'package:peer_app/presentation/whitelabel/text_constants.dart';
+import 'package:intl/intl.dart';
 
 class ChatsCardComponent extends StatelessWidget {
   const ChatsCardComponent({super.key, required this.chats});
@@ -55,22 +56,23 @@ class ChatsHeaderComponent extends StatelessWidget {
     // if lastMessageTime is null, display an empty text widget instead, otherwise display the formatted time
     Widget lastMessageTimeWidget = lastMessageTime == null
         ? const Text("")
-        : Text(
-            '${lastMessageTime!.month}.${lastMessageTime!.day}, ${lastMessageTime!.hour}:${lastMessageTime!.minute.toString().padLeft(2, '0')}', // Using string interpolation
-            style: const TextStyle(color: CustomColors.secondaryTextColor),
-          );
+        : FormattedDateWidget(
+            dateTime: lastMessageTime!,
+            style: const TextStyle(color: CustomColors.secondaryTextColor));
 
     return Padding(
       padding: const EdgeInsets.symmetric(
           horizontal: AppPaddings.large, vertical: AppPaddings.small),
       child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           // Display the user avatar (profile image)
           AvatarComponent(imageUrl: user.profileImageUrl),
           Padding(
-            padding: const EdgeInsets.symmetric(horizontal: AppPaddings.large),
+            padding: const EdgeInsets.fromLTRB(AppPaddings.small, 0, 0, 0),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Text(
                   // Display the user name
@@ -92,9 +94,12 @@ class ChatsHeaderComponent extends StatelessWidget {
           Column(
             crossAxisAlignment: CrossAxisAlignment.end,
             children: [
-              // Text Widget that displays the time of the last message
-              lastMessageTimeWidget,
-              theCircle, // Widget to display the notification count
+              Padding(
+                padding: const EdgeInsets.only(bottom: AppPaddings.medium),
+                child:
+                    lastMessageTimeWidget, // Custom widget to display the formatted date
+              ),
+              theCircle, // Custom widget to display the notification count
             ],
           ),
         ],
@@ -116,41 +121,74 @@ class NotificationCircle extends StatefulWidget {
 class _NotificationCircleState extends State<NotificationCircle> {
   @override
   Widget build(BuildContext context) {
+    // Determine the text to display based on the notificationCount
+    final String displayText = widget.notificationCount > 99
+        ? "99+" // Currently not working becuase of the fixed size of the container
+        : widget.notificationCount.toString();
+
+    // Determine the font size based on the length of displayText
+    final double fontSize;
+    if (displayText.length > 2) {
+      fontSize = 9.0; // For text length greater than 2 (e.g., "99+")
+    } else if (displayText.length > 1) {
+      fontSize = 12.0; // For text length greater than 1 (e.g., "10" to "99")
+    } else {
+      fontSize = 14.0; // For text length of 1 (e.g., "0" to "9")
+    }
+
     return Container(
-      width: AppDimensions.notificationIndicator,
+      width:
+          AppDimensions.notificationIndicator, // Keep the circle size constant
       height: AppDimensions.notificationIndicator,
       decoration: const BoxDecoration(
         color: CustomColors.primaryColor,
         shape: BoxShape.circle,
       ),
-      child: Center(
-        child: Text(
-          '${widget.notificationCount}', // Access the notificationCount from above using the widget property
-          style: const TextStyle(
-            color: CustomColors.lightTextColor,
-            fontWeight: FontWeight.bold,
-          ),
+      alignment: Alignment.center,
+      child: Text(
+        displayText,
+        style: TextStyle(
+          color: CustomColors.lightTextColor,
+          fontWeight: FontWeight.bold,
+          fontSize: fontSize, // Use the dynamically determined font size
         ),
+        textAlign: TextAlign.center,
       ),
     );
   }
 }
 
-class LikeCountComponent extends StatelessWidget {
-  const LikeCountComponent({super.key});
+class FormattedDateWidget extends StatelessWidget {
+  final DateTime dateTime;
+  final TextStyle? style; // Optional TextStyle parameter
+
+  const FormattedDateWidget({
+    Key? key,
+    required this.dateTime,
+    this.style, // Initialize the optional style parameter
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        CustomIconButton(
-          onPressed: () {},
-          sizeType: SizeType.small,
-          icon: IconLibrary.heart,
-        ),
-        const SizedBox(width: AppPaddings.small),
-        Text("12", style: Theme.of(context).textTheme.titleLarge),
-      ],
-    );
+    DateTime now = DateTime.now();
+    DateTime today = DateTime(now.year, now.month, now.day);
+    DateTime aWeekAgo = today.subtract(const Duration(days: 7));
+    DateTime startOfThisYear = DateTime(now.year);
+    String formattedDate;
+
+    if (dateTime.isAfter(aWeekAgo)) {
+      // If the message is less than a week ago, format as "Friday, 23:56"
+      formattedDate = DateFormat('EEEE, HH:mm').format(dateTime);
+    } else if (dateTime.isAfter(startOfThisYear)) {
+      // If it's longer than a week ago, but it's still within the same year, format as "March, 11"
+      formattedDate = DateFormat('MMMM, dd').format(dateTime);
+    } else {
+      // If the message was delivered last year or an earlier year, format as "April 20, 2022"
+      formattedDate = DateFormat('MMMM dd, yyyy').format(dateTime);
+    }
+
+    // Use the provided style if available, otherwise default to the bodyLarge style of the current theme
+    return Text(formattedDate,
+        style: style ?? Theme.of(context).textTheme.bodyLarge);
   }
 }
