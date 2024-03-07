4727e5b2de09347fc3d3cc86089f0ccaab48375a
JDKahle <jonnykahle@yahoo.de>
2024-03-07 15:01:25 +0100

    renaming

--- lib/presentation/pages/single_chat_page/chat_page.dart
+++ lib/presentation/pages/single_chat_page/chat_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:peer_app/data/models/chat_message_model.dart';
 import 'package:peer_app/presentation/pages/BasePage.dart';
 import 'package:peer_app/presentation/pages/single_chat_page/widgets/chat_history_view.dart';
+import 'package:peer_app/presentation/whitelabel/components/appbars/secondary_appbar.dart';
 
 class ChatPage extends StatelessWidget {
   const ChatPage({Key? key, this.chatId, this.chatMsg}) : super(key: key);
@@ -11,11 +12,12 @@ class ChatPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BasePage(
-      appBar: AppBar(
-        title: const Text('Chat'),
+    return const BasePage(
+      appBar: SecondaryAppbar(
+        title: 'Pamela Jones',
+        // title: const Text('Chat'),
       ),
-      child: const ChatHistoryView(),
+      child: ChatHistoryView(),
     );
   }
 }
