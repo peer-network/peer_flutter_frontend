e75da4019c389a719cbe5dd3797d97cf79d7b2f9
JDKahle <jonnykahle@yahoo.de>
2024-04-05 11:07:18 +0200

    expand forward to list on tap

--- lib/presentation/pages/share_post/share_to_listed_contacts_view.dart
+++ lib/presentation/pages/share_post/share_to_listed_contacts_view.dart
@@ -7,10 +7,19 @@ import 'package:peer_app/presentation/whitelabel/components/loading_and_error/lo
 import 'package:peer_app/presentation/whitelabel/constants.dart';
 import 'package:provider/provider.dart';
 
-class ShareToListedContactsView extends StatelessWidget {
+class ShareToListedContactsView extends StatefulWidget {
   final Set<String> activeContacts;
 
-  const ShareToListedContactsView({super.key, required this.activeContacts});
+  const ShareToListedContactsView({Key? key, required this.activeContacts})
+      : super(key: key);
+
+  @override
+  _ShareToListedContactsViewState createState() =>
+      _ShareToListedContactsViewState();
+}
+
+class _ShareToListedContactsViewState extends State<ShareToListedContactsView> {
+  bool _showAllContacts = false;
 
   @override
   Widget build(BuildContext context) {
@@ -18,39 +27,55 @@ class ShareToListedContactsView extends StatelessWidget {
 
     if (contactsProvider.isLoading) {
       return LoadingComponent(
-        onRefresh: () => contactsProvider.fetchContacts(),
-      );
+          onRefresh: () => contactsProvider.fetchContacts());
     } else if (contactsProvider.error != null) {
       return ErrorComponent(error: contactsProvider.error!);
     } else {
       List<ContactModel> filteredContacts = contactsProvider.contacts
-          .where((contact) => activeContacts.contains(contact.id))
+          .where((contact) => widget.activeContacts.contains(contact.id))
           .toList();
 
+      List<Widget> contactWidgets = [];
       String displayText;
+
       if (filteredContacts.isEmpty) {
         displayText = "No contacts selected";
-      } else if (filteredContacts.length == 1) {
-        displayText = "Forward to: ${filteredContacts.first.contact.username}";
-      } else if (filteredContacts.length == 2) {
-        displayText =
-            "Forward to: ${filteredContacts[0].contact.username}, ${filteredContacts[1].contact.username}";
+        contactWidgets.add(Text(displayText,
+            style:
+                TextStyle(fontSize: 16, color: CustomColors.primaryTextColor)));
       } else {
-        int additionalContacts = filteredContacts.length - 2;
-        displayText =
-            "Forward to: ${filteredContacts[0].contact.username}, ${filteredContacts[1].contact.username}, +$additionalContacts";
+        for (int i = 0; i < filteredContacts.length; i++) {
+          if (i < 2 || _showAllContacts) {
+            contactWidgets.add(Text(filteredContacts[i].contact.username,
+                style: TextStyle(
+                    fontSize: 16, color: CustomColors.primaryTextColor)));
+            if (i < filteredContacts.length - 1) {
+              contactWidgets.add(Text(", ",
+                  style: TextStyle(
+                      fontSize: 16, color: CustomColors.primaryTextColor)));
+            }
+          }
+        }
+
+        if (filteredContacts.length > 2 && !_showAllContacts) {
+          int additionalContacts = filteredContacts.length - 2;
+          contactWidgets.add(GestureDetector(
+            onTap: () => setState(() => _showAllContacts = true),
+            child: Text(
+              "+$additionalContacts",
+              style: TextStyle(
+                  fontSize: 16,
+                  color: CustomColors.primaryTextColor,
+                  decoration: TextDecoration.underline),
+            ),
+          ));
+        }
       }
 
       return Padding(
         padding: const EdgeInsets.all(AppPaddings.medium),
         child: Wrap(
-          children: [
-            Text(
-              displayText,
-              style:
-                  TextStyle(fontSize: 16, color: CustomColors.primaryTextColor),
-            ),
-          ],
+          children: contactWidgets,
         ),
       );
     }
