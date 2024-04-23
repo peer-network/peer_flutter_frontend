a2e2ada504c410aaa69abf05b41959770feb2033
JDKahle <jonnykahle@yahoo.de>
2024-04-23 11:39:35 +0200

    chat funktioniert dynamisch. neue chat messages werden erst nach neu laden der app angezeigt. die current user id muss noch noch dynamisch an den provider übergeben werden. -> jetzt liegt es ersmal am backend

--- lib/presentation/pages/single_chat_page/widgets/messaging_bottom_nav_bar.dart
+++ lib/presentation/pages/single_chat_page/widgets/messaging_bottom_nav_bar.dart
@@ -72,7 +72,105 @@
 // Bottom navigation bar for messaging
 // This is a stateful widget that contains a text input field and a send button.
 
+// import 'package:flutter/material.dart';
+// import 'package:peer_app/data/models/chat_model.dart';
+// import 'package:peer_app/data/provider/chat_provider.dart';
+// import 'package:peer_app/presentation/whitelabel/components/custom_toast.dart';
+// import 'package:peer_app/presentation/whitelabel/components/input/input_field_component.dart';
+// import 'package:peer_app/presentation/whitelabel/components/navbars/base_bottom_navbar.dart';
+// import 'package:peer_app/presentation/whitelabel/constants.dart';
+// import 'package:provider/provider.dart';
+
+// class MessagingBottomNavBar extends StatefulWidget {
+//   const MessagingBottomNavBar.messagingBottomNavBar(
+//       {Key? key, required this.chat})
+//       : super(key: key);
+
+//   final ChatModel chat;
+
+//   @override
+//   State<MessagingBottomNavBar> createState() => _MessagingBottomNavBarState();
+// }
+
+// class _MessagingBottomNavBarState extends State<MessagingBottomNavBar> {
+//   final TextEditingController _controller = TextEditingController();
+//   bool _isSendButtonVisible = false;
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     _controller.addListener(_onTextChanged);
+//   }
+
+//   @override
+//   void dispose() {
+//     _controller.dispose();
+//     super.dispose();
+//   }
+
+//   void _onTextChanged() {
+//     setState(() {
+//       _isSendButtonVisible = _controller.text.isNotEmpty;
+//     });
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return BaseBottomNavigationBar(
+//       height: null,
+//       child: Padding(
+//         padding: const EdgeInsets.symmetric(
+//             horizontal: AppPaddings.small, vertical: AppPaddings.tiny),
+//         child: Row(
+//           mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//           children: [
+//             Expanded(
+//               child: InputFieldComponent(
+//                 controller: _controller,
+//                 minLines: 1,
+//                 maxLines: 4,
+//                 hintText: "Type a message",
+//               ),
+//             ),
+//             if (_isSendButtonVisible)
+//               IconButton(
+//                 icon: Icon(Icons.send, color: Theme.of(context).primaryColor),
+//                 onPressed: () {
+//                   //TODO: Implement send functionality here
+//                   print("Sending: ${_controller.text}");
+//                   sendTextMessage(
+//                       // "1e05f953-3f9b-488b-acf2-8486d37f6009", // chatId
+//                       );
+//                 },
+//               ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+
+// //////////////////////// START //////////////////////////
+//   void sendTextMessage() {
+//     final chatProvider = Provider.of<ChatProvider>(context, listen: false);
+//     final creatorId = chatProvider.currentUserId;
+//     final chatId = chatProvider.chats.first.id;
+//     chatProvider.createChatMessage(
+//       {"content": _controller.text, "chatId": chatId, "senderId": creatorId},
+//     ).then((_) {
+//       if (chatProvider.error != null) {
+//         CustomToast.showErrorToast(chatProvider.error!);
+//       } else {
+//         CustomToast.showSuccessToast("Chat Message erfolgreich erstellt");
+//       }
+//     });
+//   }
+// //////////////////////// END //////////////////////////
+// }
+
+// File: messaging_bottom_nav_bar.dart
+
 import 'package:flutter/material.dart';
+import 'package:peer_app/data/models/chat_model.dart'; // Ensure ChatModel is imported
 import 'package:peer_app/data/provider/chat_provider.dart';
 import 'package:peer_app/presentation/whitelabel/components/custom_toast.dart';
 import 'package:peer_app/presentation/whitelabel/components/input/input_field_component.dart';
@@ -81,7 +179,10 @@ import 'package:peer_app/presentation/whitelabel/constants.dart';
 import 'package:provider/provider.dart';
 
 class MessagingBottomNavBar extends StatefulWidget {
-  const MessagingBottomNavBar.messagingBottomNavBar({Key? key})
+  final ChatModel? chat; // Field to hold ChatModel
+
+  // Update the named constructor to accept the ChatModel
+  const MessagingBottomNavBar.messagingBottomNavBar({Key? key, this.chat})
       : super(key: key);
 
   @override
@@ -131,14 +232,7 @@ class _MessagingBottomNavBarState extends State<MessagingBottomNavBar> {
             if (_isSendButtonVisible)
               IconButton(
                 icon: Icon(Icons.send, color: Theme.of(context).primaryColor),
-                onPressed: () {
-                  //TODO: Implement send functionality here
-                  print("Sending: ${_controller.text}");
-                  sendTextMessage(
-                    "1e05f953-3f9b-488b-acf2-8486d37f6009", // chatId
-                    "c05a6e6e-5365-40ca-b2d5-29af9f1cb1c6", // creatorId
-                  );
-                },
+                onPressed: sendTextMessage,
               ),
           ],
         ),
@@ -146,9 +240,10 @@ class _MessagingBottomNavBarState extends State<MessagingBottomNavBar> {
     );
   }
 
-//////////////////////// START //////////////////////////
-  void sendTextMessage(String chatId, String creatorId) {
+  void sendTextMessage() {
     final chatProvider = Provider.of<ChatProvider>(context, listen: false);
+    final creatorId = chatProvider.currentUserId;
+    final chatId = widget.chat!.id; // Does the Null needs to be handled here?
     chatProvider.createChatMessage(
       {"content": _controller.text, "chatId": chatId, "senderId": creatorId},
     ).then((_) {
@@ -159,5 +254,4 @@ class _MessagingBottomNavBarState extends State<MessagingBottomNavBar> {
       }
     });
   }
-//////////////////////// END //////////////////////////
 }
