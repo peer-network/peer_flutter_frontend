cda4b895d94f65d22591129fa108daee7a9ceef0
JDKahle <jonnykahle@yahoo.de>
2024-04-26 14:55:11 +0200

    the text of the search element is centered again

--- lib/presentation/whitelabel/components/search_elements/text_search.dart
+++ lib/presentation/whitelabel/components/search_elements/text_search.dart
@@ -1,3 +1,4 @@
+// File: search_field.dart
 import 'package:flutter/material.dart';
 import 'package:peer_app/presentation/whitelabel/colors.dart';
 import 'package:peer_app/presentation/whitelabel/constants.dart';
@@ -12,23 +13,21 @@ class SearchField extends StatelessWidget {
     return SizedBox(
       height: AppDimensions.searchElementContainerHight,
       width: double.infinity,
-      child: Expanded(
-        child: TextField(
-          decoration: InputDecoration(
-            hintText: 'Suchen ...',
-            suffixIcon: const Icon(Icons.search),
-            border: OutlineInputBorder(
-                borderRadius: AppBorders.defaultRadius,
-                borderSide:
-                    Theme.of(context).inputDecorationTheme.outlineBorder!),
-            alignLabelWithHint: true,
-            contentPadding: const EdgeInsets.fromLTRB(
-              AppPaddings.medium,
-              AppPaddings.tiny,
-              AppPaddings.small,
-              AppPaddings.tiny,
-            ), // Adjust padding to center the text and icon
+      child: TextField(
+        controller: controller,
+        style: TextStyle(height: 1.0),
+        decoration: InputDecoration(
+          hintText: 'Suchen ...',
+          suffixIcon: const Icon(Icons.search),
+          border: OutlineInputBorder(
+              borderRadius: AppBorders.defaultRadius,
+              borderSide: BorderSide.none),
+          isDense: true,
+          contentPadding: const EdgeInsets.symmetric(
+            vertical: (AppDimensions.searchElementContainerHight - 20) / 2,
+            horizontal: AppPaddings.medium,
           ),
+          alignLabelWithHint: true,
         ),
       ),
     );
