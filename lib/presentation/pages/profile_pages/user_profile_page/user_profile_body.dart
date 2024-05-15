3ac00dcae254172c8e73157609c6d02d42b9ff34
JDKahle <jonnykahle@yahoo.de>
2024-05-15 16:31:27 +0200

    cleaning

--- lib/presentation/pages/profile_pages/user_profile_page/user_profile_body.dart
+++ lib/presentation/pages/profile_pages/user_profile_page/user_profile_body.dart
@@ -1,44 +1,3 @@
-// import 'package:flutter/material.dart';
-// import 'package:peer_app/core/types/post_types.dart';
-// import 'package:peer_app/data/models/user_model.dart';
-// import 'package:peer_app/presentation/pages/profile_pages/components/profile_image_post_section.dart';
-// import 'package:peer_app/presentation/pages/profile_pages/components/profile_image_section.dart';
-// import 'package:peer_app/presentation/pages/profile_pages/components/profile_info_section.dart';
-// import 'package:peer_app/presentation/pages/profile_pages/user_profile_page/components/profile_text_post_section.dart';
-// import 'package:peer_app/presentation/whitelabel/constants.dart';
-
-// class UserProfileBody extends StatelessWidget {
-//   const UserProfileBody({super.key, required this.user});
-
-//   final UserModel user;
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return SingleChildScrollView(
-//       child: Column(
-//         children: [
-//           ProfileImageSection(imgUrl: user.imageUrl),
-//           const SizedBox(height: AppPaddings.large),
-//           ProfileInfoSection(user: user),
-//           const SizedBox(height: AppPaddings.large),
-//           ProfileImagePostSection(
-//             posts: user.posts
-//                 .where((post) => post.postType == PostType.image)
-//                 .toList(),
-//           ),
-//           const SizedBox(height: AppPaddings.large),
-//           ProfileTextPostSection(
-//             posts: user.posts
-//                 .where((post) => post.postType == PostType.text)
-//                 .toList(),
-//             user: user,
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
-
 import 'package:flutter/material.dart';
 import 'package:peer_app/core/types/post_types.dart';
 import 'package:peer_app/data/models/user_model.dart';
@@ -55,8 +14,6 @@ class UserProfileBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // print('Number of user posts: ${user.posts.length}');
-
     return SingleChildScrollView(
       child: Column(
         children: [
@@ -64,7 +21,6 @@ class UserProfileBody extends StatelessWidget {
           const SizedBox(height: AppPaddings.large),
           ProfileInfoSection(user: user),
           const SizedBox(height: AppPaddings.large),
-          // Text('user.posts: ${user.posts}'), // Debugging only
           ProfileImagePostSection(
             posts: user.posts
                 .where((post) => post.postType == PostType.image)
