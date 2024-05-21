b5cff2da72463864767e6658d60804277d6b8469
JDKahle <jonnykahle@yahoo.de>
2024-05-21 16:37:24 +0200

    the detail view of a image post displays basic comments now

--- lib/presentation/pages/profile_pages/components/detailed_image_page.dart
+++ lib/presentation/pages/profile_pages/components/detailed_image_page.dart
@@ -1,5 +1,39 @@
+// import 'package:flutter/material.dart';
+// import 'package:peer_app/data/models/feed_model.dart';
+
+// class DetailedImagePage extends StatelessWidget {
+//   final FeedModel post;
+
+//   const DetailedImagePage({super.key, required this.post});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('Image Details'),
+//       ),
+//       body: Padding(
+//         padding: const EdgeInsets.all(16.0),
+//         child: Column(
+//           crossAxisAlignment: CrossAxisAlignment.start,
+//           children: [
+//             Image.network(post.imageUrls[0]),
+//             const SizedBox(height: 16),
+//             // Text(
+//             //   'Description: ${post.description}',
+//             //   style: const TextStyle(fontSize: 16),
+//             // ),
+//             // Add more details here
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
 import 'package:peer_app/data/models/feed_model.dart';
+import 'package:peer_app/data/models/feed_comment.dart';
 
 class DetailedImagePage extends StatelessWidget {
   final FeedModel post;
@@ -12,20 +46,83 @@ class DetailedImagePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Image Details'),
       ),
-      body: Padding(
+      body: SingleChildScrollView(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Image.network(post.imageUrls[0]),
             const SizedBox(height: 16),
-            // Text(
-            //   'Description: ${post.description}',
-            //   style: const TextStyle(fontSize: 16),
-            // ),
-            // Add more details here
+            Text(
+              post.title,
+              style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 8),
+            Text(
+              'Description: ${post.content}',
+              style: const TextStyle(fontSize: 16),
+            ),
+            const SizedBox(height: 16),
+            const Text('Comments:',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+            _buildComments(post.comments),
           ],
         ),
+      ),
+    );
+  }
+
+  Widget _buildComments(List<CommentModel> comments) {
+    return Column(
+      children: comments.map((comment) => _buildCommentItem(comment)).toList(),
+    );
+  }
+
+  Widget _buildCommentItem(CommentModel comment) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              CircleAvatar(
+                backgroundImage: NetworkImage(comment.user.imageUrl ??
+                    "FALLBACK VALUE"), // Assuming UserModel has a profilePictureUrl field
+                radius: 16,
+              ),
+              const SizedBox(width: 8),
+              Text(
+                comment.user.name,
+                style: const TextStyle(fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+          const SizedBox(height: 4),
+          Text(
+            comment.content,
+            style: const TextStyle(fontSize: 16),
+          ),
+          const SizedBox(height: 4),
+          Row(
+            children: [
+              Text(
+                '${comment.createdAt}',
+                style: const TextStyle(fontSize: 12, color: Colors.grey),
+              ),
+              const SizedBox(width: 8),
+              Text(
+                'Likes: ${comment.likeCount}',
+                style: const TextStyle(fontSize: 12, color: Colors.grey),
+              ),
+            ],
+          ),
+          if (comment.comments.isNotEmpty)
+            Padding(
+              padding: const EdgeInsets.only(left: 16.0, top: 8.0),
+              child: _buildComments(comment.comments),
+            ),
+        ],
       ),
     );
   }
--- lib/presentation/pages/profile_pages/components/profile_image_post_section.dart
+++ lib/presentation/pages/profile_pages/components/profile_image_post_section.dart
@@ -29,11 +29,50 @@
 //   }
 // }
 
+// import 'package:flutter/material.dart';
+// import 'package:peer_app/data/models/feed_model.dart';
+// import 'package:peer_app/presentation/whitelabel/components/image_container/custom_cached_network_image.dart';
+// import 'package:peer_app/presentation/whitelabel/constants.dart';
+// import 'package:peer_app/presentation/pages/profile_pages/components/detailed_image_page.dart';
+
+// class ProfileImagePostSection extends StatelessWidget {
+//   const ProfileImagePostSection({super.key, required this.posts});
+
+//   final List<FeedModel> posts;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return GridView.builder(
+//       shrinkWrap: true,
+//       physics: const NeverScrollableScrollPhysics(),
+//       itemCount: posts.length,
+//       gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+//         crossAxisCount: 3,
+//         crossAxisSpacing: AppMargins.tiny,
+//         mainAxisSpacing: AppMargins.tiny,
+//       ),
+//       itemBuilder: (context, index) {
+//         return GestureDetector(
+//           onTap: () {
+//             Navigator.push(
+//               context,
+//               MaterialPageRoute(
+//                 builder: (context) => DetailedImagePage(post: posts[index]),
+//               ),
+//             );
+//           },
+//           child: CustomCachedNetworkImage(imageUrl: posts[index].imageUrls[0]),
+//         );
+//       },
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
 import 'package:peer_app/data/models/feed_model.dart';
 import 'package:peer_app/presentation/whitelabel/components/image_container/custom_cached_network_image.dart';
 import 'package:peer_app/presentation/whitelabel/constants.dart';
-import 'package:peer_app/presentation/pages/profile_pages/components/detailed_image_page.dart';
+import 'package:peer_app/presentation/pages/profile_pages/components/detailed_image_page.dart'; // Import the detailed image page
 
 class ProfileImagePostSection extends StatelessWidget {
   const ProfileImagePostSection({super.key, required this.posts});
