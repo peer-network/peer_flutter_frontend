838dbcc19a6afda436262357d0139bc11ab0f7bc
JDKahle <jonnykahle@yahoo.de>
2024-05-28 16:03:23 +0200

    ...

--- lib/presentation/pages/profile_pages/components/detailed_image_page/components/comments_single_components/like_button_component.dart
+++ lib/presentation/pages/profile_pages/components/detailed_image_page/components/comments_single_components/like_button_component.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:peer_app/data/models/feed_comment_model.dart';
-import 'package:peer_app/data/models/user_model.dart';
 import 'package:peer_app/data/services/user_service.dart';
 import 'package:peer_app/presentation/whitelabel/colors.dart';
+import 'package:peer_app/presentation/whitelabel/components/buttons/custom_icon_button.dart';
 import 'package:peer_app/presentation/whitelabel/constants.dart';
-import 'package:peer_app/presentation/whitelabel/icon_library.dart'; // Make sure to import IconLibrary
+import 'package:peer_app/presentation/whitelabel/icon_library.dart';
+import 'package:peer_app/presentation/whitelabel/components/types/size_types.dart';
 
 class LikeButtonComponent extends StatefulWidget {
   const LikeButtonComponent(
@@ -30,30 +31,24 @@ class _LikeButtonComponentState extends State<LikeButtonComponent> {
 
   @override
   Widget build(BuildContext context) {
-    return IconButton(
-      icon: Icon(
-        isLiked ? IconLibrary.star : IconLibrary.heart,
-        color: isLiked
-            ? Theme.of(context).brightness == Brightness.light
-                ? LightColors.iconCompany
-                : DarkColors.iconCompany
-            : widget.textColor,
-      ),
-      onPressed: () async {
-        setState(() {
-          isLoading = true;
-        });
-        final bool success = await UserService().toggleLike(widget.comment.id);
-        setState(() {
-          isLoading = false;
-          if (success) {
-            isLiked = !isLiked;
-          }
-        });
-      },
-      iconSize: AppDimensions.buttonHeightSmall,
-      splashColor: Colors.transparent,
-      highlightColor: Colors.transparent,
-    );
+    return CustomIconButton(
+        icon: isLiked ? IconLibrary.star : IconLibrary.heart,
+        onPressed: () async {
+          setState(() {
+            isLoading = true;
+          });
+          final bool success =
+              await UserService().toggleLike(widget.comment.id);
+          setState(() {
+            isLoading = false;
+            if (success) {
+              isLiked = !isLiked;
+            }
+          });
+        },
+        sizeType: SizeType.small,
+        color: Theme.of(context).brightness == Brightness.light
+            ? LightColors.iconCompany
+            : DarkColors.iconCompany);
   }
 }
