58de3ba89a31d11574f382500d691774a468956d
Jeremi Kilimnik <banik1103.dev@gmail.com>
2024-05-28 12:43:24 +0200

    ability to reload image post on error

--- lib/presentation/pages/peer_page/widgets/feed_component/feed_content/feed_content_image_widget.dart
+++ lib/presentation/pages/peer_page/widgets/feed_component/feed_content/feed_content_image_widget.dart
@@ -1,20 +1,62 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-class FeedContentImageWidget extends StatelessWidget {
+class FeedContentImageWidget extends StatefulWidget {
   final List<String> imageUrls;
   const FeedContentImageWidget({super.key, required this.imageUrls});
+
+  @override
+  State<FeedContentImageWidget> createState() => _FeedContentImageWidgetState();
+}
+
+class _FeedContentImageWidgetState extends State<FeedContentImageWidget> {
+  final ValueNotifier<int> _reloadNotifier = ValueNotifier<int>(0);
+
+  @override
+  void dispose() {
+    _reloadNotifier.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       height: MediaQuery.of(context).size.width, // Assuming square images
       child: PageView.builder(
-        itemCount: imageUrls.length,
+        itemCount: widget.imageUrls.length,
         controller: PageController(
             viewportFraction:
                 1), // Adjust the viewportFraction for partially visible next image
         itemBuilder: (context, index) {
-          return Image.network(imageUrls[index], fit: BoxFit.fitWidth);
+          return ValueListenableBuilder(
+              valueListenable: _reloadNotifier,
+              builder: (context, value, child) {
+                return Image.network(
+                  widget.imageUrls[index],
+                  fit: BoxFit.fitWidth,
+                  filterQuality: FilterQuality.medium,
+                  loadingBuilder: (context, child, loadingProgress) {
+                    return loadingProgress == null
+                        ? child
+                        : Center(
+                            child: CircularProgressIndicator(
+                              value: loadingProgress.expectedTotalBytes != null
+                                  ? loadingProgress.cumulativeBytesLoaded /
+                                      loadingProgress.expectedTotalBytes!
+                                  : null,
+                            ),
+                          );
+                  },
+                  errorBuilder: (context, error, stackTrace) {
+                    return GestureDetector(
+                      onTap: () => _reloadNotifier.value++,
+                      child: Center(
+                        child: Text('Failed to load image. Tap to retry.'),
+                      ),
+                    );
+                  },
+                );
+              });
         },
       ),
     );
