a0de87383574c791218fca39d7bb31322d1d0aaa
JDKahle <jonnykahle@yahoo.de>
2024-05-29 12:26:52 +0200

    comments in own dummy file

---
+++ lib/data/dummy_response/dummy_post_comments.dart
@@ -0,0 +1,100 @@
+const comment_alex_post_1 = [
+  {
+    "id": "1",
+    "content": "This is a fantastic view!",
+    "postId": 1,
+    "userId": 2,
+    "likeCount": 10,
+    "createdAt": "2024-04-30T09:00:00.000Z",
+    "isLiked": false,
+    "creator": {
+      "id": "2",
+      "name": "John Doe",
+      "imageUrl": "https://randomuser.me/api/portraits/men/1.jpg"
+    },
+    "comments": [
+      {
+        "id": "2",
+        "content": "Absolutely breathtaking!",
+        "postId": 1,
+        "userId": 3,
+        "likeCount": 3,
+        "createdAt": "2024-04-30T10:00:00.000Z",
+        "isLiked": true,
+        "creator": {
+          "id": "3",
+          "name": "Jane Doe",
+          "imageUrl":
+              "https://storage.googleapis.com/wnr-ai/uploads/chat/ai/avatar/c3de9b8b4ea5836aaff8e88b362d5ce4/v512_c3de9b8b4ea5836aaff8e88b362d5ce4.png"
+        },
+        "comments": [
+          {
+            "id": "2",
+            "content": "Comment Layer 3.",
+            "postId": 1,
+            "userId": 3,
+            "likeCount": 3,
+            "createdAt": "2024-04-30T10:00:00.000Z",
+            "isLiked": false,
+            "creator": {
+              "id": "3",
+              "name": "Jane Doe",
+              "imageUrl":
+                  "https://storage.googleapis.com/wnr-ai/uploads/chat/ai/avatar/c3de9b8b4ea5836aaff8e88b362d5ce4/v512_c3de9b8b4ea5836aaff8e88b362d5ce4.png"
+            },
+            "comments": [
+              {
+                "id": "2",
+                "content":
+                    "Comment Layer 4. It was a great experience. I would love to visit again. Maybe next summer. Maybe never. Who knows?",
+                "postId": 1,
+                "userId": 3,
+                "likeCount": 3,
+                "createdAt": "2024-04-30T10:00:00.000Z",
+                "isLiked": false,
+                "creator": {
+                  "id": "3",
+                  "name": "Jane Doe",
+                  "imageUrl":
+                      "https://storage.googleapis.com/wnr-ai/uploads/chat/ai/avatar/c3de9b8b4ea5836aaff8e88b362d5ce4/v512_c3de9b8b4ea5836aaff8e88b362d5ce4.png"
+                },
+                "comments": []
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "id": "3",
+    "content": "Love this place!",
+    "postId": 1,
+    "userId": 4,
+    "likeCount": 5,
+    "createdAt": "2024-04-30T11:00:00.000Z",
+    "isLiked": false,
+    "creator": {
+      "id": "4",
+      "name": "Emily Smith",
+      "imageUrl": "https://randomuser.me/api/portraits/women/2.jpg"
+    },
+    "comments": [
+      {
+        "id": "4",
+        "content": "Me too! Visited last summer.",
+        "postId": 1,
+        "userId": 5,
+        "likeCount": 2,
+        "createdAt": "2024-04-30T12:00:00.000Z",
+        "isLiked": false,
+        "creator": {
+          "id": "5",
+          "name": "Chris Johnson",
+          "imageUrl": "https://randomuser.me/api/portraits/men/2.jpg"
+        },
+        "comments": []
+      }
+    ]
+  }
+];
--- lib/data/dummy_response/dummy_user_by_id.dart
+++ lib/data/dummy_response/dummy_user_by_id.dart
@@ -1,3 +1,5 @@
+import 'package:peer_app/data/dummy_response/dummy_post_comments.dart';
+
 const dummyUserById = {
   "data": [
     {
@@ -166,106 +168,7 @@ const dummyUserById = {
                 "https://whyy.org/wp-content/uploads/2022/08/2022-08-15-e-lee-alex-smith-philadelphia-black-gay-comic-book-768x530.jpeg"
           },
           "catId": "2",
-          "comments": [
-            {
-              "id": "1",
-              "content": "This is a fantastic view!",
-              "postId": 1,
-              "userId": 2,
-              "likeCount": 10,
-              "createdAt": "2024-04-30T09:00:00.000Z",
-              "isLiked": false,
-              "creator": {
-                "id": "2",
-                "name": "John Doe",
-                "imageUrl": "https://randomuser.me/api/portraits/men/1.jpg"
-              },
-              "comments": [
-                {
-                  "id": "2",
-                  "content": "Absolutely breathtaking!",
-                  "postId": 1,
-                  "userId": 3,
-                  "likeCount": 3,
-                  "createdAt": "2024-04-30T10:00:00.000Z",
-                  "isLiked": true,
-                  "creator": {
-                    "id": "3",
-                    "name": "Jane Doe",
-                    "imageUrl":
-                        "https://storage.googleapis.com/wnr-ai/uploads/chat/ai/avatar/c3de9b8b4ea5836aaff8e88b362d5ce4/v512_c3de9b8b4ea5836aaff8e88b362d5ce4.png"
-                  },
-                  "comments": [
-                    {
-                      "id": "2",
-                      "content": "Comment Layer 3.",
-                      "postId": 1,
-                      "userId": 3,
-                      "likeCount": 3,
-                      "createdAt": "2024-04-30T10:00:00.000Z",
-                      "isLiked": false,
-                      "creator": {
-                        "id": "3",
-                        "name": "Jane Doe",
-                        "imageUrl":
-                            "https://storage.googleapis.com/wnr-ai/uploads/chat/ai/avatar/c3de9b8b4ea5836aaff8e88b362d5ce4/v512_c3de9b8b4ea5836aaff8e88b362d5ce4.png"
-                      },
-                      "comments": [
-                        {
-                          "id": "2",
-                          "content":
-                              "Comment Layer 4. It was a great experience. I would love to visit again. Maybe next summer. Maybe never. Who knows?",
-                          "postId": 1,
-                          "userId": 3,
-                          "likeCount": 3,
-                          "createdAt": "2024-04-30T10:00:00.000Z",
-                          "isLiked": false,
-                          "creator": {
-                            "id": "3",
-                            "name": "Jane Doe",
-                            "imageUrl":
-                                "https://storage.googleapis.com/wnr-ai/uploads/chat/ai/avatar/c3de9b8b4ea5836aaff8e88b362d5ce4/v512_c3de9b8b4ea5836aaff8e88b362d5ce4.png"
-                          },
-                          "comments": []
-                        }
-                      ]
-                    }
-                  ]
-                }
-              ]
-            },
-            {
-              "id": "3",
-              "content": "Love this place!",
-              "postId": 1,
-              "userId": 4,
-              "likeCount": 5,
-              "createdAt": "2024-04-30T11:00:00.000Z",
-              "isLiked": false,
-              "creator": {
-                "id": "4",
-                "name": "Emily Smith",
-                "imageUrl": "https://randomuser.me/api/portraits/women/2.jpg"
-              },
-              "comments": [
-                {
-                  "id": "4",
-                  "content": "Me too! Visited last summer.",
-                  "postId": 1,
-                  "userId": 5,
-                  "likeCount": 2,
-                  "createdAt": "2024-04-30T12:00:00.000Z",
-                  "isLiked": false,
-                  "creator": {
-                    "id": "5",
-                    "name": "Chris Johnson",
-                    "imageUrl": "https://randomuser.me/api/portraits/men/2.jpg"
-                  },
-                  "comments": []
-                }
-              ]
-            }
-          ],
+          "comments": comment_alex_post_1,
           "amountClicks": 0,
           "amountComments": 0,
           "imageUrls": [
