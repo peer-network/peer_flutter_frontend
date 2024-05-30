5b8f5ca2f09d666df711c627525304ea797e4227
JDKahle <jonnykahle@yahoo.de>
2024-05-30 11:36:19 +0200

    can click on detail view for text

--- lib/presentation/pages/profile_pages/user_profile_page/components/profile_text_post_section.dart
+++ lib/presentation/pages/profile_pages/user_profile_page/components/profile_text_post_section.dart
@@ -1,3 +1,33 @@
+// // import 'package:flutter/material.dart';
+// // import 'package:peer_app/data/models/post_model.dart';
+// // import 'package:peer_app/data/models/user_model.dart';
+// // import 'package:peer_app/presentation/pages/profile_pages/user_profile_page/components/small_text_post_card.dart';
+
+// // class ProfileTextPostSection extends StatelessWidget {
+// //   const ProfileTextPostSection(
+// //       {super.key, required this.posts, required this.user});
+
+// //   final List<PostModel> posts;
+// //   final UserModel user;
+
+// //   @override
+// //   Widget build(BuildContext context) {
+// //     return SingleChildScrollView(
+// //       scrollDirection: Axis.horizontal,
+// //       child: Row(
+// //         children: posts
+// //             .map(
+// //               (post) => SmallTextPostCard(
+// //                 user: user,
+// //                 post: post,
+// //               ),
+// //             )
+// //             .toList(),
+// //       ),
+// //     );
+// //   }
+// // }
+
 // import 'package:flutter/material.dart';
 // import 'package:peer_app/data/models/post_model.dart';
 // import 'package:peer_app/data/models/user_model.dart';
@@ -32,6 +62,7 @@ import 'package:flutter/material.dart';
 import 'package:peer_app/data/models/post_model.dart';
 import 'package:peer_app/data/models/user_model.dart';
 import 'package:peer_app/presentation/pages/profile_pages/user_profile_page/components/small_text_post_card.dart';
+import 'package:peer_app/presentation/routing/routes/page_routes.dart';
 
 class ProfileTextPostSection extends StatelessWidget {
   const ProfileTextPostSection(
@@ -45,14 +76,19 @@ class ProfileTextPostSection extends StatelessWidget {
     return SingleChildScrollView(
       scrollDirection: Axis.horizontal,
       child: Row(
-        children: posts
-            .map(
-              (post) => SmallTextPostCard(
-                user: user,
-                post: post,
-              ),
-            )
-            .toList(),
+        children: posts.map((post) {
+          return GestureDetector(
+            onTap: () {
+              Navigator.of(context).push(
+                DetailedPostPageRoute(post, post.id),
+              );
+            },
+            child: SmallTextPostCard(
+              user: user,
+              post: post,
+            ),
+          );
+        }).toList(),
       ),
     );
   }
