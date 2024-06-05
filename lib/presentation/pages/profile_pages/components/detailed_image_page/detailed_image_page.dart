1b5cc2cf5a47e3de43807b1eaa0c4ca9eeb8c3fc
JDKahle <jonnykahle@yahoo.de>
2024-06-05 16:13:43 +0200

    beide detailed_image_page.dart dateien die veraltet sind auskommentiert

--- lib/presentation/pages/profile_pages/components/detailed_image_page/detailed_image_page.dart
+++ lib/presentation/pages/profile_pages/components/detailed_image_page/detailed_image_page.dart
@@ -1,31 +1,33 @@
-import 'package:flutter/material.dart';
-import 'package:peer_app/data/models/post_model.dart';
+//>>>>>>>>>>>> LÖSCHEN (Veraltet) <<<<<<<<<<<<
 
-import 'package:peer_app/presentation/pages/BasePage.dart';
-import 'package:peer_app/presentation/pages/profile_pages/components/detailview_all_post_types_page/components/comments/comments_logic/first_layer_comment.dart';
-import 'package:peer_app/presentation/whitelabel/constants.dart';
+// import 'package:flutter/material.dart';
+// import 'package:peer_app/data/models/post_model.dart';
 
-class DetailedImagePage extends StatelessWidget {
-  final PostModel post;
-  final String imageUrl;
+// import 'package:peer_app/presentation/pages/BasePage.dart';
+// import 'package:peer_app/presentation/pages/profile_pages/components/detailview_all_post_types_page/components/comments/comments_logic/first_layer_comment.dart';
+// import 'package:peer_app/presentation/whitelabel/constants.dart';
 
-  const DetailedImagePage(
-      {super.key, required this.post, required this.imageUrl});
+// class DetailedImagePage extends StatelessWidget {
+//   final PostModel post;
+//   final String imageUrl;
 
-  @override
-  Widget build(BuildContext context) {
-    return BasePage(
-        appBar: AppBar(
-          title: const Text('Image Details'),
-        ),
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Hero(tag: 'post-$imageUrl', child: Image.network(imageUrl)),
-              const SizedBox(height: AppPaddings.extraLarge),
-              FirstLayerComment(comments: post.comments),
-            ],
-          ),
-        ));
-  }
-}
+//   const DetailedImagePage(
+//       {super.key, required this.post, required this.imageUrl});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return BasePage(
+//         appBar: AppBar(
+//           title: const Text('Image Details'),
+//         ),
+//         child: SingleChildScrollView(
+//           child: Column(
+//             children: [
+//               Hero(tag: 'post-$imageUrl', child: Image.network(imageUrl)),
+//               const SizedBox(height: AppPaddings.extraLarge),
+//               FirstLayerComment(comments: post.comments),
+//             ],
+//           ),
+//         ));
+//   }
+// }
