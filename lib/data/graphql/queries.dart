a16163080aa0c658a95702a1a0b6fe56aaa8a7be
Jeremi Kilimnik <banik1103.dev@gmail.com>
2024-08-30 15:23:15 +0200

    cleanup

--- lib/data/graphql/queries.dart
+++ lib/data/graphql/queries.dart
@@ -2,58 +2,6 @@ import 'package:graphql_flutter/graphql_flutter.dart';
 import 'package:peer_app/data/graphql/fragments.dart';
 
 class Queries {
-  //! posts are not fetched in user in getAllPosts
-  //? should comments be pulled when fetching posts? comments {} are excluded for now
-  //? verficationToken - is this necessary in the user model when pulling posts?
-
-  // old query
-  /*static final posts = gql("""
-query GetAllPosts {
-    getAllPosts {
-        id
-        contentType
-        title
-        media
-        mediaDescription
-        createdAt
-        updatedAt
-        amountLikes
-        amountViews
-        amountComments
-        amountTrending
-        isLiked
-        isViewed
-        isReported
-        isDisliked
-        isSaved
-        user {
-            id
-            username
-            email
-            imgUrl
-            biograph
-            verified
-            verificationToken
-            amountPosts
-            amountFollowed
-            amountFollower
-            isFollowing
-        }
-        gemsTotal
-        gemsToday
-        gemsAllTimeHigh
-        gemsLikes
-        gemsViews
-        gemsShares
-        likesPerDay{
-          date
-          amount
-        }
-        
-    }
-}
-""");*/
-
   static final posts = gql("""
 query Getallposts {
     getallposts {
@@ -81,63 +29,6 @@ query Getallposts {
 }
 """);
 
-// query GetUserById {
-//     getUserById(id: "0c4762a8-0a39-11ef-b7f2-e89c25791d89") {
-//         id
-//         username
-//         email
-//         img
-//         biograph
-//         isPrivate
-//         amountPosts
-//         amountFollowed
-//         amountFollower
-//         isFollowed
-//     }
-// }
-
-  //? verificationToken - is this necessary in the user model?
-  //? should comments be pulled when fetching posts when pulling user? comments {} in posts {}, comments {} are excluded for now
-
-  static final getUserById = gql("""
-query GetUserById(\$id: String!) {
-    getUserById(id: \$id) {
-        id
-        username
-        email
-        imgUrl
-        verified
-        biograph
-        isPrivate
-        amountPosts
-        amountFollowed
-        amountFollower
-        isFollowing
-        createdAt
-        updatedAt
-        verificationToken
-        posts {
-            id
-            contentType
-            title
-            media
-            mediaDescription
-            createdAt
-            updatedAt
-            amountLikes
-            amountViews
-            amountComments
-            amountTrending
-            isLiked
-            isViewed
-            isReported
-            isDisliked
-            isSaved
-        }
-    }
-}
-""");
-
   // erste Zeile: welche Variablen (+ datentyp) brauchst du um zu bekommen was du willst
   // zweite Zeile: was bekommst du zurück (was willst du genau haben aus welchem table)
   static final wallet = gql("""
@@ -147,24 +38,14 @@ query Wallet(\$user_id: Int) {
   }
 }
 """);
-}
 
-// query GetPost(\$postId: String!) { ----- changed to "ID!"
-const String getPostById = """
-query GetPost(\$postId: ID!) {
-  getPost(id: \$postId) {
-        comments {
-            id
-            content
-            createdAt
-            user {
-                id
-                username
-                img
-            }
-            postId
-            userId
-        }
+  /// query to handshake with server and get user id
+  static final hello = gql("""
+query Hello {
+    hello {
+        context
+        currentuserid
     }
 }
-""";
+""");
+}
